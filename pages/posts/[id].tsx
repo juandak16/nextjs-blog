f77bcd9e00109ebdb0c1cc2fc115613c2c02c9d5
David Hidalgo <juandak16@gmail.com>
2023-10-26 12:05:09 -0400

    feat: config typescript

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,49 @@
+import Layout from "../../components/Layout";
+import Head from "next/head";
+import { PostData, getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/Date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  if (!params || !params.id) {
+    return {
+      props: {},
+    };
+  }
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+const Post = ({ postData }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          {postData.date && <Date dateString={postData.date} />}
+        </div>
+        {postData.contentHtml && (
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        )}
+      </article>
+    </Layout>
+  );
+};
+
+export default Post;
